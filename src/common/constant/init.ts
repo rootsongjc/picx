96f6e26a1d48fb61a853319afec19239773d8641
Jimmy Song <rootsongjc@gmail.com>
2024-06-18 11:06:42 +0800

    chore: update INIT_REPO_NAME constant to use 'images' instead of 'picx-images-hosting'

--- src/common/constant/init.ts
+++ src/common/constant/init.ts
@@ -1,5 +1,5 @@
 export const PICX_REPO_NAME = 'XPoet/picx'
-export const INIT_REPO_NAME = 'picx-images-hosting'
+export const INIT_REPO_NAME = 'images'
 export const INIT_REPO_DESC = 'PicX images hosting repository'
 export const INIT_REPO_BARNCH = 'master'
 export const GH_PAGES = 'gh-pages'
